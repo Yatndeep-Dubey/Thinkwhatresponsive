188b4bfd3498d0c0458bd2d4d3ebb58372e60389
Yatndeep-Dubey <dubeyyatndeep@gmail.com>
2023-03-13 01:39:07 +0530

    make changes in index.js

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
 const express = require("express");
-const mongoose= require('mongoose')
 const app = express();
-const mgRouter=require("./routes/mgroute");
-const mgController= require("./controller/mgcontroller")
-const adminModel =require("./model/adminModel")
-const userModel = require("./model/users")
 const bodyParser= require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-mongoose.connect("mongodb://localhost:27017/ManagementSystem",{
-useNewUrlParser: true,
-useUnifiedTopology: true,
-}
-)
-const db= mongoose.connection;
-db.on("error", (error) => console.log(error));
-db.once("open", () => console.log("Database Connected"));
+
 app.listen(8080, () => {
     console.log(`Event app listening at http://localhost:8080`);
   });
@@ -33,7 +21,4 @@ app.use(express.static('public'))
  
 app.use('/assets',express.static('assets'));
 
- app.post("/",mgController.insertadmin);
- app.get("/admin",mgController.getadmin);
- app.post("/user",mgController.insertuser);
  app.use("/public/images",express.static('./public/images'));
